224b262fa0a77b9e9c8b81cb427b0298b6b19f24
Binyamin Klein <beklein@acad.jct.ac.il>
2023-12-27 18:52:17 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program8589.cs
@@ -0,0 +1,20 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome8589();
+            Welcome0954();
+            Console.ReadKey();
+        }
+        static partial void Welcome0954();
+
+        private static void Welcome8589()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application.", name);
+        }
+    }
+}
