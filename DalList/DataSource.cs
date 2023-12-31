8ee4c86429ddd3b59815109196d57f73ab901737
Eli Isaacs <eisaacs@g.jct.ac.il>
2023-12-31 18:51:03 +0200

    Stuff

---
+++ DalList/DataSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dal;
+
+internal static class DataSource
+{
+	internal static class Config {
+        internal static Status _status { get; set; }
+        internal static DateTime _startDate { get; set; }
+        internal static DateTime _endDate { get; set; }
+    }
+
+	internal static List<DO.Engineer> Engineers { get; } = new();
+	internal static List<DO.Dependency> Dependencies { get; } = new();
+	internal.static List<DO.Task> Tasks { get; } = new();
+
+	public DataSource()
+	{
+	}
+}
