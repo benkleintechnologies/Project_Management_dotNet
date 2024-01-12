ab97fe9238028538f832470c003fc468f308dcbc
Binyamin Klein <beklein@acad.jct.ac.il>
2024-01-12 10:17:25 +0200

    Added DalList

---
+++ DalList/DalList.cs
@@ -0,0 +1,16 @@
+﻿namespace Dal;
+using DalApi;
+
+/// <summary>
+/// DalList Class which implements the IDal Interface with functions which return the implementation of the appropriate object type.
+/// </summary>
+sealed public class DalList : IDal
+{
+    public IConfig Config => new ConfigImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
+
+    public IDependency Dependency => new DependencyImplementation();
+}
