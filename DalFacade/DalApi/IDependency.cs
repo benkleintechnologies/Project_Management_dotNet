e0d4bf996769655d0eeefe8fb7b8a036c1f4eb2d
Binyamin Klein <beklein@acad.jct.ac.il>
2024-01-12 10:01:57 +0200

    Stage 2 Perek 2

--- DalFacade/DalApi/IDependency.cs
+++ DalFacade/DalApi/IDependency.cs
@@ -4,12 +4,7 @@ using DO;
 /// <summary>
 /// Interface to access Dependencies using CRUD
 /// </summary>
-public interface IDependency
+public interface IDependency : ICrud<Dependency>
 {
-    int Create(Dependency item); // Creates new entity object in DAL
-    Dependency? Read(int id); // Reads entity object by its ID
-    List<Dependency> ReadAll(); // stage 1 only, Reads all entity objects // Updates entity object
-    void Update(Dependency item); // Updates entity object
-    void Delete(int id); // Deletes an object by its Id
-    void Reset(); // Reset list of dependencies
+    
 }
