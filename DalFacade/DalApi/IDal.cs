ba31aff339b6602b0feab789c29100c804843b93
Eli Isaacs <eisaacs@g.jct.ac.il>
2024-01-12 10:11:03 +0200

    Created IDal

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,13 @@
+﻿namespace DalApi;
+
+/// <summary>
+/// IDal interface that allows readonly access to each of the subinterfaces
+/// </summary>
+public interface IDal
+{
+    IConfig Config { get; }
+    IEngineer Engineer { get; }
+    ITask Task { get; }
+    IDependency Dependency { get; }
+    
+}
