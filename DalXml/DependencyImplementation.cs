ecd8f4ba2de41e640f9c43d1090875c383840295
Binyamin Klein <beklein@acad.jct.ac.il>
2024-01-14 19:26:24 +0200

    Started working on CRUD Implementations for XML

--- DalXml/DependencyImplementation.cs
+++ DalXml/DependencyImplementation.cs
@@ -3,43 +3,117 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 internal class DependencyImplementation : IDependency
 {
-    readonly string s_engineers_xml = "engineers";
+    readonly string s_dependencies_xml = "dependencies";
 
     public int Create(Dependency item)
     {
-        throw new NotImplementedException();
+        int _id = Config.NextDependencyId;
+        Dependency _dependency = item with { id = _id };
+        XElement _dependencies = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        _dependencies.Add(_dependency);
+        XMLTools.SaveListToXMLElement(_dependencies, s_dependencies_xml);
+        return _id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Dependency? _dependency = Read(id);
+        if (_dependency == null)
+        {
+            throw new DalDoesNotExistException($"Object of type Dependency with identifier {id} does not exist");
+        }
+        else
+        {
+            //Get list of dependencies
+            XElement _dependencies = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+            //Find the dependency with id
+            XElement _dependencyElement = _dependencies.Elements("Dependency").FirstOrDefault(e => (int)e.Attribute("id")! == id)!;
+
+            if (_dependencyElement == null)
+            {
+                throw new DalDoesNotExistException($"Object of type Dependency with identifier {id} does not exist");
+            }
+            //Set it as deleted
+            _dependencyElement.Element("active")?.SetValue(false);
+            XMLTools.SaveListToXMLElement(_dependencies, s_dependencies_xml);
+        }
     }
 
     public Dependency? Read(int id)
     {
-        throw new NotImplementedException();
+        //Get liost of dependencies
+        XElement _dependencies = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        //Find the dependency with id
+        XElement _dependencyElement = _dependencies.Elements("Dependency").FirstOrDefault(e => (int)e.Attribute("id")! == id)!;
+        //Create Dependency object from the element
+
+        //TODO: Parse the data
+        //
+        //
+        //
+        return null;
     }
 
     public Dependency? Read(Func<Dependency, bool> filter)
     {
+        //TODO: Implement
+        //
+        //
+        //
+        //
+
         throw new NotImplementedException();
     }
 
     public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
     {
+        //TODO: Implement
+        //
+        //
+        //
+        //
+
         throw new NotImplementedException();
     }
 
     public void Reset()
     {
-        throw new NotImplementedException();
+        XElement _dependencies = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        _dependencies.RemoveAll();
+        XMLTools.SaveListToXMLElement(_dependencies, s_dependencies_xml);
     }
 
     public void Update(Dependency item)
     {
-        throw new NotImplementedException();
+        Dependency? _old = Read(item.id);
+        if (_old != null)
+        {
+            //Get list of dependencies
+            XElement _dependencies = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+            //Find the dependency with id
+            XElement _dependencyElement = _dependencies.Elements("Dependency").FirstOrDefault(e => (int)e.Attribute("id")! == _old.id)!;
+
+            if (_dependencyElement == null)
+            {
+                throw new DalDoesNotExistException($"Object of type Dependency with identifier {id} does not exist");
+            }
+            
+            //update it
+            //_dependencyElement.Element(
+            //TODO: Implement
+            //
+            //
+            //
+
+            XMLTools.SaveListToXMLElement(_dependencies, s_dependencies_xml);
+        }
+        else
+        {
+            throw new DalDoesNotExistException($"Object of type Dependency with identifier {item.id} does not exist");
+        }
     }
 }
--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -3,43 +3,115 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using Task = DO.Task;
 
+/// <summary>
+/// Implementation of Task Interface to access Tasks from XML
+/// </summary>
 internal class TaskImplementation : ITask
 {
-    readonly string s_engineers_xml = "engineers";
+    readonly string s_tasks_xml = "tasks"; //Name of XML file to access
 
+    /// <summary>
+    /// Add Task to XML File
+    /// </summary>
+    /// <param name="item">The Task to add</param>
+    /// <returns>The ID of the Task</returns>
     public int Create(Task item)
     {
-        throw new NotImplementedException();
+        int _id = Config.NextTaskId;
+        Task _task = item with { id = _id };
+        List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        _tasks.Add(_task);
+        XMLTools.SaveListToXMLSerializer<Task>(_tasks, s_tasks_xml);
+        return _id;
     }
 
+    /// <summary>
+    /// Delete a Task from the XML File
+    /// </summary>
+    /// <param name="id">ID of the Task to delete</param>
+    /// <exception cref="DalDoesNotExistException">Thrown if there is no Task with this ID in the database</exception>
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        Task? _task = Read(id);
+        if (_task == null)
+        {
+            throw new DalDoesNotExistException($"Object of type Task with identifier {id} does not exist");
+        }
+        else
+        {
+            List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+            _tasks.Remove(_task);
+            Task _newTask = _task with { active = false };
+            _tasks.Add(_task);
+            XMLTools.SaveListToXMLSerializer<Task>(_tasks, s_tasks_xml);
+        }
     }
 
+    /// <summary>
+    /// Retreive a Task from the XML File by ID
+    /// </summary>
+    /// <param name="id">ID of the Task</param>
+    /// <returns>The Task object requested</returns>
     public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        return _tasks.FirstOrDefault(item => item.id == id && item.active);
     }
 
+    /// <summary>
+    /// Retreive an Engineer from the XML File based on a filter
+    /// </summary>
+    /// <param name="filter">The criteria of the requested Engineer</param>
+    /// <returns>The Engineer object requested</returns>
     public Task? Read(Func<Task, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        return _tasks.Where(item => item.active).FirstOrDefault(filter);
     }
 
+    /// <summary>
+    /// Retreive all Tasks from the XML File
+    /// </summary>
+    /// <param name="filter">Optional filter to limit list</param>
+    /// <returns>Requested Enumerable of Tasks</returns>
     public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+        if (filter == null)
+        {
+            return _tasks.Where(item => item.active);
+        }
+        return _tasks.Where(filter).Where(item => item.active);
     }
 
+    /// <summary>
+    /// Removes all Tasks from the XML File
+    /// </summary>
     public void Reset()
     {
-        throw new NotImplementedException();
+        XMLTools.SaveListToXMLSerializer<Task>(new List<Task>(), s_tasks_xml);
     }
 
+    /// <summary>
+    /// Updates a Task in the XML File
+    /// </summary>
+    /// <param name="item">New Task information</param>
+    /// <exception cref="DalDoesNotExistException">Thrown if no Task with the same ID exists</exception>
     public void Update(Task item)
     {
-        throw new NotImplementedException();
+        Task? _old = Read(item.id);
+        if (_old != null)
+        {
+            List<Task> _tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+            _tasks.Remove(_old);
+            _tasks.Add(item);
+            XMLTools.SaveListToXMLSerializer<Task>(_tasks, s_tasks_xml);
+        }
+        else
+        {
+            throw new DalDoesNotExistException($"Object of type Task with identifier {item.id} does not exist");
+        }
     }
 }
