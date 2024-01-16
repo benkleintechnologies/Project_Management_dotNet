8c1963b4b55f2f9f5b1e92f5ed9549ce8802042f
Eli Isaacs <eisaacs@g.jct.ac.il>
2024-01-16 15:56:49 +0200

    Created DalXML

---
+++ DalXml/DalXML.cs
@@ -0,0 +1,17 @@
+﻿namespace Dal;
+using DalApi;
+
+// stage 3
+/// <summary>
+/// Returns new implementation object for each of the entities
+/// </summary>
+sealed public class DalXML : IDal
+{
+    public IConfig Config => new ConfigImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
+
+    public IDependency Dependency => new DependencyImplementation();
+}
