7fca5f7af05fb348f10cd3ace7e081cfcee80d09
Binyamin Klein <beklein@acad.jct.ac.il>
2024-01-24 16:38:49 +0200

    Added implementation classes

---
+++ BL/BlImplementation/TaskImplementation.cs
@@ -0,0 +1,34 @@
+﻿namespace BlImplementation;
+using BlApi;
+using BO;
+using System;
+using System.Collections.Generic;
+
+internal class TaskImplementation : ITask
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+    public void addTask(Task Task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void deleteTask(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Task> getListOfTasks(Func<Task, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task getTask(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void updateTask(Task Task)
+    {
+        throw new NotImplementedException();
+    }
+}
