474497c2d19143ccebcf12afffcc455764bad3db
Eli Isaacs <eisaacs@g.jct.ac.il>
2024-01-24 16:51:39 +0200

    Made Name, Email, and Cost not nullable in Engineer

--- BL/BO/Engineer.cs
+++ BL/BO/Engineer.cs
@@ -6,10 +6,10 @@
 public class Engineer
 {
     public int Id { get; init; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
     public EngineerExperience Experience { get; set; }
-    public double? Cost { get; set; }
+    public double Cost { get; set; }
     public TaskInEngineer? Task { get; set; }
     public override string ToString() => this.ToStringProperty();
 }
