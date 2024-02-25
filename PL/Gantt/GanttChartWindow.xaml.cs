ec99a65ff3d4d70257e3cd48dd918ffd8b92284a
Binyamin Klein <beklein@acad.jct.ac.il>
2024-02-25 17:14:57 +0200

    Got rid of name in XAML

--- PL/Gantt/GanttChartWindow.xaml.cs
+++ PL/Gantt/GanttChartWindow.xaml.cs
@@ -33,17 +33,14 @@ public partial class GanttChartWindow : Window
     public GanttChartWindow()
     {
         InitializeComponent();
-    }
-    private void AddRow()
-    {
-        DataRow dr = DataTable.NewRow();
-        dr["Test"] = "Wert1";
-        DataTable.Rows.Add(dr);
+        DataContext = this;
     }
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        List<TaskInList> tasks = s_bl.Task.GetListOfTasks().ToList();
+        List<BO.TaskInList> tasks = s_bl.Task.GetListOfTasks().ToList();
+        List<BO.Milestone> milestones = s_bl.Milestone.GetListOfMilestones().Select(m => s_bl.Milestone.GetMilestone(m.ID)).ToList();
+        List<DateTime> milestoneEndDates = milestones.Select(m => m.ActualEndDate ?? m.ProjectedEndDate ?? DateTime.MinValue).ToList();
 
         DataTable = new DataTable();
 
@@ -62,12 +59,22 @@ public partial class GanttChartWindow : Window
         }
         
         double diffResult = (endDate - startDate).TotalDays;
+        //Add a column for the task name
         DataTable.Columns.Add("TASK NAME");
+        //Add a column for each day between the start and end date
         for (int i = 0; i <= diffResult; i++)
         {
+            //Add a column for each milestone
+            if (milestoneEndDates.Any(m => m.Date == startDate.AddDays(i).Date))
+            {
+                DataTable.Columns.Add(milestones.First(m => m.ActualEndDate == startDate.AddDays(i) || m.ProjectedEndDate == startDate.AddDays(i)).Name);
+            }
+            //TODO: Change the width of the column just added to 20
+
             DataTable.Columns.Add(startDate.AddDays(i).ToString("MMM dd yy"));
         }
 
+        //Add a row for each task
         DataRow dr = DataTable.NewRow();
         for (int i = 0; i < tasks.Count(); i++)
         {
@@ -81,6 +88,7 @@ public partial class GanttChartWindow : Window
                 this.Close();
             }
 
+            //Go through each column and add the task status to the corresponding dates
             for (int j = 0; j < DataTable.Columns.Count; j++)
             {
                 if (DataTable.Columns[j].ToString() == "TASK NAME")
@@ -92,9 +100,12 @@ public partial class GanttChartWindow : Window
                             DataTable.Rows.Add(dr);
                             dr = DataTable.NewRow();
                         }
-                        dr[j] = tasks[i].Name!.ToString();
+                        dr[j] = tasks[i].Name!.ToString(); //Add the task name to the row
                     }
-
+                }
+                else if (milestones.Any(m => m.Name == DataTable.Columns[j].ToString())) //if the column is a milestone
+                {
+                    dr[j] = milestones.First(m => m.Name == DataTable.Columns[j].ToString()).Status.ToString();
                 }
                 else if (Convert.ToDateTime(DataTable.Columns[j].ToString(), new CultureInfo("en-US")) >= taskStartDate && Convert.ToDateTime(DataTable.Columns[j].ToString(), new CultureInfo("en-US")) <= taskEndDate) //if the date is between the start and end date of the task
                 {
@@ -104,6 +115,15 @@ public partial class GanttChartWindow : Window
 
         }
         DataTable.Rows.Add(dr);
-        FooBar1.ItemsSource = DataTable.DefaultView;
+        DataView = DataTable.DefaultView;
+    }
+
+    private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Ensure that the DataView is not null before setting it as the ItemsSource
+        if (DataView != null)
+        {
+            ((DataGrid)sender).ItemsSource = DataView;
+        }
     }
 }
